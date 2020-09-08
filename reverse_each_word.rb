e81ea6338a795b391ab4c0678d45b9faa5ac4cc9
Will Vetter <vetter.william@gmail.com>
2020-09-08 20:30:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(sentence)
-  my_array = []
-  sentence.split.each do |word|
-    my_array << word.reverse
+  sentence.split.collect do |word|
+    word.reverse
   end
-  my_array.join(" ")
 end
